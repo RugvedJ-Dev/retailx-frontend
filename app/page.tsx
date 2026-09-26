255e3870df12f53c40f6d751fed73e673898818f
Rugved <rugvedjadhao122004.dev@gmail.com>
2026-09-26 22:15:43 +0530

    fixed the font issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         fill
         alt="hero"
       />
+      <Navbar/>
     </>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full h-20 text-white flex justify-around items-center font-inter">
+      <div>RetailX AI</div>
+      <div>How it works?</div>
+      <div>Pricing</div>
+      <div>About Developers</div>
+    </nav>
+  );
+}
